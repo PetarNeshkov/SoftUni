035b6b4f5af70cf806fa3fbb38d0a861f4b783f6
Petar Neshkov <petio567@abv.bg>
2020-12-10 11:53:45 +0200

    added a test

---
+++ C#-Fundamentals/Programming Tests/Bonus Scoring System/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentCount = int.Parse(Console.ReadLine());
+            int lecturesCount = int.Parse(Console.ReadLine());
+            int bonus = int.Parse(Console.ReadLine());
+            double totalBonus = 0;
+            double MaxBonus = 0;
+            int MaxStudentAttendances = 0;
+            for (int i = 0; i < studentCount; i++)
+            {
+                int attendancesForStudent = int.Parse(Console.ReadLine());
+                totalBonus = (attendancesForStudent * 1.00 / lecturesCount * 1.00);
+                totalBonus *= (5 + bonus);
+                if (totalBonus > MaxBonus)
+                {
+                    MaxBonus = totalBonus;
+                    MaxStudentAttendances = attendancesForStudent;
+                }
+            }
+            Console.WriteLine($"Max Bonus: {Math.Ceiling(MaxBonus)}.");
+            Console.WriteLine($"The student has attended {MaxStudentAttendances} lectures.");
+        }
+    }
+}
