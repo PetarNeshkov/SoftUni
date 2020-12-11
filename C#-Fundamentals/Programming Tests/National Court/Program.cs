3497668ecd3496685561c8221adb6d3a0a804fa4
Petar Neshkov <petio567@abv.bg>
2020-12-11 13:33:28 +0200

    added a test

---
+++ C#-Fundamentals/Programming Tests/National Court/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int firstEmployee = int.Parse(Console.ReadLine());
+            int secondEmpleyee = int.Parse(Console.ReadLine());
+            int thirdEmployee = int.Parse(Console.ReadLine());
+            int MaxWork = firstEmployee + secondEmpleyee + thirdEmployee;
+            int peopleCount = int.Parse(Console.ReadLine());
+            int hours = 0;
+            int sum = 0;
+            while (sum < peopleCount)
+            {
+                hours++;
+                sum += MaxWork;
+                if (hours % 4 == 0)
+                {
+                    sum -= MaxWork;
+                    continue;
+                }
+            }
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
