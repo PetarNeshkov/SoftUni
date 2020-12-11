933327bbf4014f37da93f5db83f9960b9c0e3d3c
Petar Neshkov <petio567@abv.bg>
2020-12-11 13:33:02 +0200

    added a test

---
+++ C#-Fundamentals/Programming Tests/Shopping List/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<string> products = Console.ReadLine().Split("!").ToList();
+            string input = "";
+            while ((input = Console.ReadLine()) != "Go Shopping!")
+            {
+                string[] commands = input.Split();
+                string action = commands[0];
+                if (action == "Urgent")
+                {
+                    string item = commands[1];
+                    if (!products.Contains(item))
+                    {
+                        products.Insert(0, item);
+                    }
+                }
+                else if (action == "Unnecessary")
+                {
+                    string item = commands[1];
+                    if (products.Contains(item))
+                    {
+                        products.Remove(item);
+                    }
+                }
+                else if (action == "Correct")
+                {
+                    string oldItem = commands[1];
+                    string newItem = commands[2];
+                    if (products.Contains(oldItem))
+                    {
+                        int IndexOFOld = products.IndexOf(oldItem);
+                        products[IndexOFOld] = newItem;
+                    }
+                }
+                else if (action == "Rearrange")
+                {
+                    string item = commands[1];
+                    if (products.Contains(item))
+                    {
+                        int IndexOFOld = products.IndexOf(item);
+                        products.RemoveAt(IndexOFOld);
+                        products.Add(item);
+                    }
+                }
+            }
+            Console.Write(String.Join(", ", products));
+        }
+    }
+}
