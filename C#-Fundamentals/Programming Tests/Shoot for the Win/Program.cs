7f549c5d574e8c65eecd1bc493f8b38a2d0903d0
Petar Neshkov <petio567@abv.bg>
2020-12-12 13:16:10 +0200

    added a test

---
+++ C#-Fundamentals/Programming Tests/Shoot for the Win/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Targets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string input = "";
+            int counter = 0;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                int index = int.Parse(input);
+                if (index >= 0 && index < Targets.Length)
+                {
+                    int CurrentNumber = Targets[index];
+                    for (int i = 0; i < Targets.Length; i++)
+                    {
+                        if (Targets[i] != -1)
+                        {
+                            if (CurrentNumber < Targets[i])
+                            {
+                                Targets[i] -= CurrentNumber;
+                            }
+                            else
+                            {
+                                Targets[i] += CurrentNumber;
+                            }
+                        }
+                    }
+                    Targets[index] = -1;
+                    counter++;
+                }
+            }
+            Console.WriteLine($"Shot targets: {counter} -> {string.Join(" ", Targets)}");
+        }
+    }
+}
