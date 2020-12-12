27e8bf6fd31357d3a3f3200b59c8915b5efcb941
Petar Neshkov <petio567@abv.bg>
2020-12-12 13:16:39 +0200

    added a test

---
+++ C#-Fundamentals/Programming Tests/Counter Strike/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int initialEnergy = int.Parse(Console.ReadLine());
+            string input = "";
+            int battles = 0;
+            while ((input = Console.ReadLine()) != "End of battle")
+            {
+                int enemyDistance = int.Parse(input);
+                if (initialEnergy < enemyDistance)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {battles} won battles and {initialEnergy} energy");
+                    return;
+                }
+                initialEnergy -= enemyDistance;
+                battles++;
+                if (battles % 3 == 0)
+                {
+                    initialEnergy += battles;
+                }
+            }
+            Console.WriteLine($"Won battles: {battles}. Energy left: {initialEnergy}");
+        }
+    }
+}
