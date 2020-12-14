43657dbcac4ce48efc7b3a4fa54da253b9c13d81
Petar Neshkov <petio567@abv.bg>
2020-12-14 10:33:11 +0200

    added a test

---
+++ C#-Fundamentals/Programming Tests/SoftUni Reception/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int workingHours = int.Parse(Console.ReadLine());
+            int hours = 0;
+            int sum = 0;
+            while (sum < workingHours)
+            {
+                sum += firstNumber + secondNumber + thirdNumber;
+                hours++;
+                if (hours % 4 == 0)
+                {
+                    sum -= firstNumber + secondNumber + thirdNumber;
+                }
+            }
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
