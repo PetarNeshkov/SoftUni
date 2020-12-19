4043da3a3a51d7e1feac4bc8115eadca489cb8e4
Petar Neshkov <petio567@abv.bg>
2020-12-19 10:49:47 +0200

    added a test

---
+++ C#-Fundamentals/Programming Tests/Destination Mapper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(?<separator>=|\/{1})(?<word>[A-Z][A-Za-z]{2,})\1";
+            int sum = 0;
+            Regex regex = new Regex(pattern);
+            var matches = regex.Matches(input);
+            List<string> destinations = new List<string>();
+            foreach (Match item in matches)
+            {
+                string destination = item.Groups["word"].Value;
+                sum += destination.Length;
+                destinations.Add(destination);
+            }
+            Console.WriteLine($"Destinations: {String.Join(", ",destinations)}");
+            Console.WriteLine($"Travel Points: {sum}");
+        }
+    }
+}
