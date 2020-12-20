a0d290687346c897dd8f499e3d12afa64f81c13b
Petar Neshkov <petio567@abv.bg>
2020-12-20 11:11:36 +0200

    added a test

---
+++ C#-Fundamentals/Programming Tests/Ad Astra/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Ad_Astra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(?<separator>\||\#{1})(?<word>[A-Za-z\s]+)\1(?<date>[\d]{2}\/[\d]{2}\/[\d]{2})\1(?<calories>[\d]+)\1";
+            Regex regex = new Regex(pattern);
+            var matches = regex.Matches(input);
+            int days = 0;
+            foreach (Match item in matches)
+            {
+                days += int.Parse(item.Groups["calories"].Value);
+            }
+            days /= 2000;
+            Console.WriteLine($"You have food to last you for: {days} days!");
+            foreach (Match item in matches)
+            {
+                string name = item.Groups["word"].Value;
+                string expDate = item.Groups["date"].Value;
+                int calories = int.Parse(item.Groups["calories"].Value);
+                Console.WriteLine($"Item: {name}, Best before: {expDate}, Nutrition: {calories}");
+            }
+        }
+    }
+}
