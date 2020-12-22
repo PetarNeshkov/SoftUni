7c00c2a8578003fcd19b8f803611b428613d74c6
Petar Neshkov <petio567@abv.bg>
2020-12-22 16:32:37 +0200

    added method exercise

---
+++ C#-Fundamentals/Methods/Orders- Methods Lab/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Orders__Methods_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOforder = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            Orders(nameOforder,quantity);
+        }
+        static void Orders(string nameOforder,int quantity)
+        {
+            double sum = 0;
+            switch (nameOforder)
+            {
+                case "water":
+                    sum = 1.00 * quantity;
+                    break;
+                case "coffee":
+                    sum = 1.50 * quantity;
+                    break;
+                case "coke":
+                    sum = 1.40 * quantity;
+                    break;
+                case "snacks":
+                    sum = 2.00 * quantity;
+                    break;
+            }
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
