d8ec3be2a7d3928d1d2c4704cf8e30611b1145ee
Petar Neshkov <petio567@abv.bg>
2020-12-22 16:33:05 +0200

    added method exercise

---
+++ C#-Fundamentals/Methods/Printing Triangle- Methods Lab/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Printing_Triangle__Methods_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintTriangle(number);
+        }
+        static void PrintTriangle(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                PrintNumbersFrom1(i);
+            }
+            for (int i = number - 1; i >= 1; i--)
+            {
+                PrintNumbersFrom1(i);
+            }
+        }
+        static void PrintNumbersFrom1(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
