4e2b63ca2b8c808a3492a5a64e36d3cd9a1e9e5e
Petar Neshkov <petio567@abv.bg>
2020-12-22 16:33:28 +0200

    added method exercise

---
+++ C#-Fundamentals/Methods/Sign of Integer Numbers- Methods Lab/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sign_of_Integer_Numbers__Methods_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int number = int.Parse(Console.ReadLine());
+            check(number);
+        }
+        static void check(int number)
+        {
+            if (number>0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number<0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
