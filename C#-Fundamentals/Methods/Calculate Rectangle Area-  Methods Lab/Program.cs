5a81b9945cc9086e458e269dcd80ce950759250a
Petar Neshkov <petio567@abv.bg>
2020-12-23 22:38:55 +0200

    added a method exercise

---
+++ C#-Fundamentals/Methods/Calculate Rectangle Area-  Methods Lab/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculate_Rectangle_Area___Methods_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine(Rectangle(width,height));
+        }
+        static int Rectangle(int width,int height)
+        {
+            return width * height;
+        }
+    }
+}
