1bc1486b0bf512c8b28f6063ae72ba62c9490dec
Petar Neshkov <petio567@abv.bg>
2020-12-24 11:52:03 +0200

    added a method exercise

---
+++ C#-Fundamentals/Methods/Repeat String -Methods Lab/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Repeat_String__Methods_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int counter = int.Parse(Console.ReadLine());
+            RepeatString(text,counter);
+        }
+        static void RepeatString(string text,int counter)
+        {
+            StringBuilder finalText = new StringBuilder();
+            for (int i = 0; i < counter; i++)
+            {
+                finalText.Append(text);
+            }
+            Console.WriteLine(finalText);
+        }
+        
+    }
+}
