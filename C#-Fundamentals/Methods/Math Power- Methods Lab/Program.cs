2404c7756b4c29a052b0d7b39b2fe338d968dace
Petar Neshkov <petio567@abv.bg>
2020-12-25 12:15:54 +0200

    added a method exercise

---
+++ C#-Fundamentals/Methods/Math Power- Methods Lab/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Math_Power__Methods_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            Console.WriteLine(MathPower(number, power));
+        }
+        static double MathPower(double number, double power)
+        {
+            double sum = 1;
+            for (int i = 0; i < power; i++)
+            {
+                sum *= number;
+            }
+            return sum;
+        }
+    }
+}
