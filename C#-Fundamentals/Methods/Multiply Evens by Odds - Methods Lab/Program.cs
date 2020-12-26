259d138cfe56c7852a51935718f2e601292f0981
Petar Neshkov <petio567@abv.bg>
2020-12-26 10:42:07 +0200

    added a method exercise

---
+++ C#-Fundamentals/Methods/Multiply Evens by Odds - Methods Lab/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Multiply_Evens_by_Odds___Methods_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            number = Math.Abs(number);
+            GetSumOfOddDigits(number);
+            GetSumOfEvenDigits(number);
+            Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+        }
+        static int GetSumOfEvenDigits(int number)
+        {
+            string stringNumber = number.ToString();
+            int sum = 0;
+            for (int i = 0; i < stringNumber.Length; i++)
+            {
+                int currentDigit = int.Parse(stringNumber[i].ToString());
+                if (currentDigit % 2 == 0)
+                {
+                    sum += currentDigit;
+                }
+            }
+            return sum;
+        }
+        static int GetSumOfOddDigits(int number)
+        {
+            string stringNumber = number.ToString();
+            int sum = 0;
+            for (int i = 0; i < stringNumber.Length; i++)
+            {
+                int currentDigit = int.Parse(stringNumber[i].ToString());
+                if (currentDigit % 2 != 0)
+                {
+                    sum += currentDigit;
+                }
+            }
+            return sum;
+        }
+        static int GetMultipleOfEvenAndOdds(int number)
+        {
+            int sum = GetSumOfEvenDigits(number) * GetSumOfOddDigits(number);
+            return sum;
+        }
+    }
+}
+
