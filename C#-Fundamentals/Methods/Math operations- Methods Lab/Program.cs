51c85eb0c8dc77af3ad78d71fb50b8c2131150c6
Petar Neshkov <petio567@abv.bg>
2020-12-28 11:28:47 +0200

    added a method exercise

---
+++ C#-Fundamentals/Methods/Math operations- Methods Lab/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Math_operations__Methods_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            char Operator = char.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(check(firstNumber, secondNumber, Operator));
+        }
+        static int check(int firstNumber, int secondNumber, char Operator)
+        {
+            int result = 0;
+            if (Operator == '+')
+            {
+                result = firstNumber + secondNumber;
+            }
+            else if (Operator == '-')
+            {
+                result = firstNumber - secondNumber;
+            }
+            else if (Operator == '*')
+            {
+                result = firstNumber * secondNumber;
+            }
+            else if (Operator == '/')
+            {
+                result = firstNumber / secondNumber;
+            }
+            return result;
+        }
+    }
+}
