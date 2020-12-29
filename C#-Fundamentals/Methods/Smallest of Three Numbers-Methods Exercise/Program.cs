1dae48e4ae2d8f3a9c121545196a233bbacb9fa5
Petar Neshkov <petio567@abv.bg>
2020-12-29 10:10:55 +0200

    added a method exercise

---
+++ C#-Fundamentals/Methods/Smallest of Three Numbers-Methods Exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Smallest_of_Three_Numbers_Methods_Exercise
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            BiggestNumber(number1, number2, number3);
+        }
+        static void BiggestNumber(int number1, int number2, int number3)
+        {
+            Console.WriteLine(Math.Min(Math.Min(number1, number2), number3));
+        }
+    }
+}
+
