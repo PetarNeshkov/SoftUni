346b4ae771f7060ab1cb6ec1d073f0fa375b8e97
Petar Neshkov <petio567@abv.bg>
2020-12-30 23:10:03 +0200

    added a method exercise

---
+++ C#-Fundamentals/Methods/Vowels Count- Methods Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Vowels_Count__Methods_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            word=word.ToLower();
+            Vowels(word);
+        }
+        static void Vowels(string word)
+        {
+            char[] Vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            int counter = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (Vowels.Contains(word[i]))
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
