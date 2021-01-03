ccad2a848bb3e80d16846ad236851da43089fb0a
Petar Neshkov <petio567@abv.bg>
2021-01-03 14:02:43 +0200

    added a method exercise

---
+++ C#-Fundamentals/Methods/Characters in Range- Methods Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Characters_in_Range__Methods_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstCharacter = char.Parse(Console.ReadLine());
+            char secondCharacter = char.Parse(Console.ReadLine());
+            CharactersInRange(firstCharacter, secondCharacter);
+        }
+        static void CharactersInRange(char firstCharacter,char secondCharacter)
+        {
+            if (firstCharacter>secondCharacter)
+            {
+                char temp;
+                temp = firstCharacter;
+                firstCharacter = secondCharacter;
+                secondCharacter = temp;
+            }
+            for (int i = firstCharacter+1; i < secondCharacter; i++)
+            {
+                Console.Write((char)i+" ");
+            }
+        }
+    }
+}
