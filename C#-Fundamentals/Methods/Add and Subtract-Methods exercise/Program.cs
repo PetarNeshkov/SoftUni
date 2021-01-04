7c5e952264052eaf5e9fd468e4716d066a3119b5
Petar Neshkov <petio567@abv.bg>
2021-01-04 14:09:28 +0200

    added a method exercise

---
+++ C#-Fundamentals/Methods/Add and Subtract-Methods exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Add_and_Subtract_Methods_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirstNumber = int.Parse(Console.ReadLine());
+            int SecondNumber = int.Parse(Console.ReadLine());
+            int ThirdNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(AddandSubtract(FirstNumber, SecondNumber, ThirdNumber));
+        }
+        static int AddandSubtract(int FirstNumber,int SecondNumber,int ThirdNumber)
+        {
+            int sum = FirstNumber + SecondNumber;
+            sum -= ThirdNumber;
+            return sum;
+        }
+    }
+}
