b74d757a0f686986f618ce1e08ac09c76862a3b4
Petar Neshkov <petio567@abv.bg>
2021-01-05 14:11:49 +0200

    added a method exercise

---
+++ C#-Fundamentals/Methods/Password Validator-Methods Exercise/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Password_Validator_Methods_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            check(password);
+        }
+        static void check(string word)
+        {
+            bool isTrue = false;
+            if (!(word.Length>=6&&word.Length<=10))
+            {
+                Console.WriteLine($"Password must be between 6 and 10 characters");
+                isTrue = true;
+            }
+            int digits = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (char.IsDigit(word[i]))
+                {
+                    digits++;
+                }
+                if (!(char.IsLetterOrDigit(word[i])))
+                {
+                    isTrue = true;
+                    Console.WriteLine($"Password must consist only of letters and digits");
+                    break;
+                }
+            }
+            if (digits<2)
+            {
+                Console.WriteLine($"Password must have at least 2 digits");
+                isTrue = true;
+            }
+            if (isTrue==false)
+            {
+                Console.WriteLine("Password is valid");
+            }
+        }
+    }
+}
