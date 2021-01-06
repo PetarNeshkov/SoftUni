35fe852e4df371b62fac8e4e269dc03906e3e392
Petar Neshkov <petio567@abv.bg>
2021-01-06 15:06:04 +0200

    added an exercise

---
+++ C#-Fundamentals/Methods/Middle Characters- Methods Exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Middle_Characters__Methods_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            MiddleCharacters(text);
+        }
+        static void MiddleCharacters(string text)
+        {
+            string input = " ";
+            if (text.Length%2==0)
+            {
+                input = text.Substring((text.Length / 2)- 1, 2);
+            }
+            else
+            {
+                input = text.Substring(text.Length / 2, 1);
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
