5b98d7b56a4679103b10718b8ac7dcac1f8a02cc
Petar Neshkov <petio567@abv.bg>
2021-01-07 21:04:31 +0200

    added a method exercise

---
+++ C#-Fundamentals/Methods/NxN Matrix-Methods Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NxN_Matrix_Methods_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            NxN(n);
+        }
+        static void NxN(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    Console.Write(n+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
