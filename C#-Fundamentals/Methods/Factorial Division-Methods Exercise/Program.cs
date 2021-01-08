15d0dd5827e219326c556e186740dc736f53814c
Petar Neshkov <petio567@abv.bg>
2021-01-08 18:41:47 +0200

    added a method exercise

---
+++ C#-Fundamentals/Methods/Factorial Division-Methods Exercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Factorial_Division_Methods_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            FactorialDivision(firstNumber, secondNumber);
+        }
+        static void FactorialDivision(int firstNumber, int secondNumber)
+        {
+            decimal Firstfactorial = 1;
+            decimal Secondfactorial = 1;
+            for (int i = 1; i <= firstNumber; i++)
+            {
+                Firstfactorial *= i;
+            }
+            for (int i = 1; i <=secondNumber; i++)
+            {
+                Secondfactorial *= i;
+            }
+            Firstfactorial /= Secondfactorial;
+            Console.WriteLine($"{Firstfactorial:F2}");
+        }
+    }
+}
