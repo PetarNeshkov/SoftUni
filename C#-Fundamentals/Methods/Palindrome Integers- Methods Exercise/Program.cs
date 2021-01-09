8d654d68cd29fd5812ed95e7c4f14a24852adf49
Petar Neshkov <petio567@abv.bg>
2021-01-09 22:22:53 +0200

    added a method exercise

---
+++ C#-Fundamentals/Methods/Palindrome Integers- Methods Exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Palindrome_Integers__Methods_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            PalindromeIntegers(command);
+        }
+        static void PalindromeIntegers(string command)
+        {
+            while (command!="END")
+            {
+                bool isPalindrome = true;
+                for (int i = 0; i < command.Length/2; i++)
+                {
+                    if (command[i]!=command[command.Length-1-i])
+                    {
+                        isPalindrome = false;
+                        Console.WriteLine("false");
+                        break;
+                    }
+                }
+                if (isPalindrome)
+                {
+                    Console.WriteLine("true");
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
