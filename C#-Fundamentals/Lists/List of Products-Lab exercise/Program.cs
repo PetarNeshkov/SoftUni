75356a0d4d40605edbd87918188b252d65bba08b
Petar Neshkov <petio567@abv.bg>
2021-01-17 00:38:20 +0200

    added a list exercise

---
+++ C#-Fundamentals/Lists/List of Products-Lab exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace _0_4TribonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = int.Parse(Console.ReadLine());
+            //List<int> list = Console.ReadLine().Split().Select(int
+            //  .Parse).ToList();
+
+            ListOfProducts(index);
+        }
+        static void ListOfProducts(int index)
+        {
+            List<string> sum = new List<string>();
+            for (int i = 0; i < index; i++)
+            {
+                string product = Console.ReadLine();
+                sum.Add(product);
+            }
+            sum.Sort();
+            for (int i = 0; i < sum.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{sum[i]}");
+
+            }
+        }
+    }
+}
