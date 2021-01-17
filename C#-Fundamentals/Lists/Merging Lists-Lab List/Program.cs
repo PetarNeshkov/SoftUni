70a44ac0757549bb70dab1f0e8600cbcc12032b6
Petar Neshkov <petio567@abv.bg>
2021-01-18 01:50:39 +0200

    added a list exercise

---
+++ C#-Fundamentals/Lists/Merging Lists-Lab List/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace _0_4TribonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> list1 = Console.ReadLine().Split().Select(int
+                .Parse).ToList();
+            List<int> list2 = Console.ReadLine().Split().Select(int
+               .Parse).ToList();
+            MergingList(list1, list2);
+        }
+        static void MergingList(List<int> list1, List<int> list2)
+        {
+            List<int> sum = new List<int>();
+            int max = Math.Max(list1.Count, list2.Count);
+            for (int i = 0; i < max; i++)
+            {
+                if (list1.Count > i)
+                {
+                    sum.Add(list1[i]);
+                }
+                if (list2.Count > i)
+                {
+                    sum.Add(list2[i]);
+
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", sum));
+        }
+
+    }
+}
