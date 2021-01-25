6e63c61f724874ec662cfb7af70feb6c709b067c
Petar Neshkov <petio567@abv.bg>
2021-01-25 19:06:51 +0200

    added a list exercise

---
+++ C#-Fundamentals/Lists/Change List-List Exercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Change_List_List_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Elements = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string input = string.Empty;
+            while ((input=Console.ReadLine())!="end")
+            {
+                string[] commands = input.Split();
+                string action = commands[0];
+                int elemements = int.Parse(commands[1]);
+                if (action=="Delete")
+                {
+                    for (int i = 0; i < Elements.Count; i++)
+                    {
+                       
+                            Elements.Remove(elemements);
+                        
+                    }
+                }
+                else if (action=="Insert")
+                {
+                    int position = int.Parse(commands[2]);
+                   
+                        Elements.Insert(position, elemements);
+                    
+                }
+            }
+            Console.WriteLine(String.Join(" ",Elements));
+        }
+    }
+}
