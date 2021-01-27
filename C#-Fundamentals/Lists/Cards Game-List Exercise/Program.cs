acf5fe6b37d8423afa3815be915cf4b0d05be224
Petar Neshkov <petio567@abv.bg>
2021-01-27 23:15:26 +0200

    added a list exercise

---
+++ C#-Fundamentals/Lists/Cards Game-List Exercise/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cards_Game_List_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Player1 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> Player2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (Player1.Count > 0 && Player2.Count > 0)
+            {
+                if (Player1[0] > Player2[0])
+                {
+                    int numberOfPlayer1 = Player1[0];
+                    int numberOfPlayer2 = Player2[0];
+                    Player1.RemoveAt(0);
+                    Player2.RemoveAt(0);
+                    Player1.Add(numberOfPlayer1);
+                    Player1.Add(numberOfPlayer2);
+
+                    if (Player2.Count == 0)
+                    {
+                        Console.WriteLine($"First player wins! Sum: {Player1.Sum()}");
+                        return;
+                    }
+                }
+                else if (Player1[0] < Player2[0])
+                {
+                    int numberOfPlayer1 = Player1[0];
+                    int numberOfPlayer2 = Player2[0];
+                    Player1.RemoveAt(0);
+                    Player2.RemoveAt(0);
+                    Player2.Add(numberOfPlayer1);
+                    Player2.Add(numberOfPlayer2);
+                    if (Player1.Count == 0)
+                    {
+                        Console.WriteLine($"Second player wins! Sum: {Player2.Sum()}");
+
+                    }
+                }
+                else
+                {
+                    Player1.RemoveAt(0);
+                    Player2.RemoveAt(0);
+                    if (Player1.Count == 0)
+                    {
+                        Console.WriteLine($"Second player wins! Sum: {Player2.Sum()}");
+                        return;
+                    }
+                    else if (Player2.Count == 0)
+                    {
+                        Console.WriteLine($"First player wins! Sum: {Player1.Sum()}");
+                        return;
+                    }
+                }
+
+            }
+
+
+        }
+    }
+}
+    
+
