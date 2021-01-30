f22b81ca334f7480325465e74b2f4a66d6977326
Petar Neshkov <petio567@abv.bg>
2021-01-30 12:45:55 +0200

    added new objects exercise

---
+++ C#-Fundamentals/Objects and classes/Day of Week-Objects and Classes - Lab/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Day_of_Week_Objects_and_Classes___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string date = Console.ReadLine();
+            DateTime Date = DateTime.ParseExact(date, "d-M-yyyy",
+                CultureInfo.InvariantCulture);
+            Console.WriteLine(Date.DayOfWeek);
+        }
+    }
+}
