5919422a196971c061c0e79cf2552128d675a62f
Petar Neshkov <petio567@abv.bg>
2021-02-01 13:59:49 +0200

    added an object exercise

---
+++ C#-Fundamentals/Objects and classes/Big Factorial-Objects and Classes - Lab/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace Big_Factorial_Objects_and_Classes___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(number));
+        }
+         static BigInteger  Factorial(int number)
+        {
+            BigInteger factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
