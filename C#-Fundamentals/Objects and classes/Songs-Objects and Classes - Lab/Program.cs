a182efc0e7989ac77e837d4e81502c12663871a0
Petar Neshkov <petio567@abv.bg>
2021-02-02 20:40:07 +0200

    added an object class

---
+++ C#-Fundamentals/Objects and classes/Songs-Objects and Classes - Lab/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Songs_Objects_and_Classes___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
+            for (int i = 0; i < counter; i++)
+            {
+                string[] data = Console.ReadLine().Split("_");
+                string TypeList = data[0];
+                string Name = data[1];
+                string Time = data[2];
+                Song song = new Song();
+                song.TypeList = TypeList;
+                song.Name = Name;
+                song.Time = Time;
+                songs.Add(song);
+            }
+            string typeList = Console.ReadLine();
+            if (typeList=="all")
+            {
+                foreach (Song song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (Song song in songs)
+                {
+                    if (song.TypeList==typeList)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+        }
+        public class Song
+        {
+            public string TypeList { get; set; }
+            public string Name { get; set; }
+            public string Time { get; set; }
+        }
+    }
+}
