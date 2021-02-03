32718a9ef06ce4b04fe8f6b8a268f74ee17c355b
Petar Neshkov <petio567@abv.bg>
2021-02-03 21:27:13 +0200

    added an object class

---
+++ C#-Fundamentals/Objects and classes/Students-Objects and Classes - Lab/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Students_Objects_and_Classes___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Students> students = new List<Students>();
+            string input = string.Empty;
+            while ((input=Console.ReadLine())!="end")
+            {
+                string[] data = input.Split();
+                string FirstName = data[0];
+                string LastName = data[1];
+                int Age = int.Parse(data[2]);
+                string City = data[3];
+                Students student = new Students();
+                student.FirstName = FirstName;
+                student.LastName = LastName;
+                student.Age = Age;
+                student.City = City;
+                students.Add(student);
+            }
+            string city = Console.ReadLine();
+            foreach (Students student in students)
+            {
+                if (city==student.City)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName}" +
+                        $" is {student.Age} years old.");
+                }
+            }
+        }
+    }
+    class Students
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
