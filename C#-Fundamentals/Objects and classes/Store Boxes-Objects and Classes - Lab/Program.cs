ed4e05d72d626798f4d70f4a7b24ce030ed704e6
Petar Neshkov <petio567@abv.bg>
2021-02-04 22:03:08 +0200

    added an object class

---
+++ C#-Fundamentals/Objects and classes/Store Boxes-Objects and Classes - Lab/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Store_Boxes_Objects_and_Classes___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Box
+    {
+        public Box()
+        {
+            ItemName = new ItemName();
+        }
+        public string SerialNumber { get; set; }
+        public string ItemName { get; set; }
+        public string ItemQuality { get; set; }
+        public double Price { get; set; }
+    }
+}
