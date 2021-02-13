bc74d516901eb5423b5254fc3322e0034f163dc1
Petar Neshkov <petio567@abv.bg>
2021-02-13 14:06:03 +0200

    added a regular expression exercise

---
+++ C#-Fundamentals/Regular expresssions/Match Phone Number-Regular Expressions - Lab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Match_Phone_Number_Regular_Expressions___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359([ -\\])2\1\d{3}\1\d{4}\b";
+            Regex regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
+
+            var matches = regex.Matches(input);
+
+            Console.Write(String.Join(", ", matches));
+        }
+    }
+}
