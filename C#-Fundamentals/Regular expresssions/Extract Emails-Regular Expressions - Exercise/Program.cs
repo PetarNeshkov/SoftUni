1fe71acadaabcd07832964dedfc43cf07c5f1bf3
Petar Neshkov <petio567@abv.bg>
2021-02-16 22:43:17 +0200

    added a regular expression exercise

---
+++ C#-Fundamentals/Regular expresssions/Extract Emails-Regular Expressions - Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Extract_Emails_Regular_Expressions___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\s(?<user>[A-Za-z0-9]+[._-]*\w*)*@(?<host>[A-Za-z]+?([.-][A-Za-z]+)*(\.[a-z]{2,}))";
+            Regex regex = new Regex(pattern);
+            string input = Console.ReadLine();
+            var matches = regex.Matches(input);
+            foreach (Match email in matches)
+            {
+                Console.WriteLine(email);
+            }
+        }
+    }
+}
