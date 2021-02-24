8137b5a9e7ae350c9b6441742d121a274541a7a5
Petar Neshkov <petio567@abv.bg>
2021-02-25 00:24:43 +0200

    added an associative array exercise

---
+++ C#-Fundamentals/Associative Arrays/Count Real Numbers-Associative Arrays -Lab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Real_Numbers_Associative_Arrays__Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double, int> counter = new Dictionary<double, int>();
+            double[] numbers =Console.ReadLine().Split().Select(double.Parse).ToArray();
+            foreach (var item in numbers)
+            {
+                if (!counter.ContainsKey(item))
+                {
+                    counter.Add(item, 1);
+                }
+                else
+                {
+                    counter[item]++;
+                }
+            }
+            foreach (var item in counter.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
