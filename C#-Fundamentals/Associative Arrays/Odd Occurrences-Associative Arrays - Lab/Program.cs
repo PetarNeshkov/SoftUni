20fcb9b03ce31727a2d3ff545c41912bec42d035
Petar Neshkov <petio567@abv.bg>
2021-02-26 00:35:03 +0200

    added an associative array exercise

---
+++ C#-Fundamentals/Associative Arrays/Odd Occurrences-Associative Arrays - Lab/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Odd_Occurrences_Associative_Arrays___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] occurences = Console.ReadLine().Split();
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+            foreach (var item in occurences)
+            {
+                if (!counts.ContainsKey(item.ToLower()))
+                {
+                    counts.Add(item.ToLower(), 1);
+                }
+                else
+                {
+                    counts[item.ToLower()]++;
+                }
+            }
+            foreach (var item in counts)
+            {
+                if (item.Value%2==1)
+                {
+                    Console.Write(item.Key+" ");
+                }
+            }
+        }
+    }
+}
