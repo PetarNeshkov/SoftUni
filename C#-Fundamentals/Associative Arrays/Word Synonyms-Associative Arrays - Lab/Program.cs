f0d191afa01e083532e0a32f338d03416f072c98
Petar Neshkov <petio567@abv.bg>
2021-02-27 00:42:02 +0200

    added an associative exercise

---
+++ C#-Fundamentals/Associative Arrays/Word Synonyms-Associative Arrays - Lab/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Word_Synonyms_Associative_Arrays___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> collection = new Dictionary<string, List<string>>();
+            for (int i = 0; i < counter; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (collection.ContainsKey(word))
+                {
+                    collection[word].Add(synonym);
+                }
+                else
+                {
+                    collection.Add(word, new List<string>());
+                    collection[word].Add(synonym);
+                }
+            }
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item.Key+" - "+String.Join(", ",item.Value) );
+            }
+        }
+    }
+}
