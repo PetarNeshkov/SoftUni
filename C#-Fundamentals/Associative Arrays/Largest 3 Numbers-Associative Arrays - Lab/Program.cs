47949a72b212a43df053ac27d261df95f27d5ab6
Petar Neshkov <petio567@abv.bg>
2021-02-28 00:35:01 +0200

    added an associative array exercise

---
+++ C#-Fundamentals/Associative Arrays/Largest 3 Numbers-Associative Arrays - Lab/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Largest_3_Numbers_Associative_Arrays___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] sorted = numbers.OrderByDescending(x => x).Take(3).ToArray();
+            Console.Write(String.Join(" ",sorted));
+        }
+    }
+}
