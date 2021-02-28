eca9df338de3c48ca1ed5118764ab151c9307dbc
Petar Neshkov <petio567@abv.bg>
2021-03-01 00:24:56 +0200

    added an associative exercise

---
+++ C#-Fundamentals/Associative Arrays/Count Chars in a String-Associative Arrays - Exercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_Chars_in_a_String_Associative_Arrays___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> counter = new Dictionary<char, int>();
+            string text = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!counter.ContainsKey(text[i]))
+                {
+                    counter.Add(text[i], 1);
+                }
+                else
+                {
+                    counter[text[i]]++;
+                }
+            }
+            foreach (var item in counter)
+            {
+                if (item.Key != ' ')
+                {
+                    Console.WriteLine($"{item.Key} -> {item.Value}");
+                }
+            }
+        }
+    }
+}
