dbc86a8e77c03257b22fed4ea1a95293c6ced51a
Petar Neshkov <petio567@abv.bg>
2021-03-02 00:12:10 +0200

    added an associative array exericise

---
+++ C#-Fundamentals/Associative Arrays/A Miner Task-Associative Arrays - Exercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_Miner_Task_Associative_Arrays___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> items = new Dictionary<string, int>();
+            string input = " ";
+            while ((input = Console.ReadLine()) != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+                if (!items.ContainsKey(input))
+                {
+                    items.Add(input, quantity);
+                }
+                else
+                {
+                    items[input] += quantity;
+                }
+            }
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
