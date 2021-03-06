9f64f6d773d2be281596d02689192225c068c125
Petar Neshkov <petio567@abv.bg>
2021-03-07 00:59:20 +0200

    added an associative array exercise

---
+++ C#-Fundamentals/Associative Arrays/Student Academy-Associative Arrays - Exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Student_Academy_Associative_Arrays___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> data = new Dictionary<string,List<double>>();
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+                if (!data.ContainsKey(name))
+                {
+                    data.Add(name, new List<double>());
+                }
+                data[name].Add(grade);
+            }
+            Dictionary<string,double> Avg =new Dictionary<string, double>();
+            foreach (var item in data)
+            {
+                Avg.Add(item.Key, item.Value.Average());
+            }
+            foreach (var student in Avg.OrderByDescending(x=>x.Value).Where(x=>x.Value>=4.50))
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value:F2}");
+            }
+        }
+    }
+}
