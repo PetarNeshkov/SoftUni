b59ad27c08945a78328ad205f71f594d480fbb80
Petar Neshkov <petio567@abv.bg>
2021-03-11 21:47:50 +0200

    added an associative array exercise

---
+++ C#-Fundamentals/Associative Arrays/Word Filter-Associative Arrays - Lab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Word_Filter_Associative_Arrays___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = Console.ReadLine().Split();
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                if (fruits[i].Length%2==0)
+                {
+                    Console.WriteLine(fruits[i]);
+                }
+            }
+        }
+    }
+}
