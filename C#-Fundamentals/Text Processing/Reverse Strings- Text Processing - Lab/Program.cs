8d8f1a399660e60c9d5db204d11f0931ddd196e7
Petar Neshkov <petio567@abv.bg>
2021-03-12 00:03:57 +0200

    added a text processing exercise

---
+++ C#-Fundamentals/Text Processing/Reverse Strings- Text Processing - Lab/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Reverse_Strings__Text_Processing___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            string input = " ";
+            while ((input=Console.ReadLine())!="end")
+            {
+                StringBuilder newString = new StringBuilder();
+                for (int i = input.Length-1; i >= 0; i--)
+                {
+                    newString.Append(input[i]);
+                }
+                Console.WriteLine($"{input} = {newString}");
+            }
+        }
+    }
+}
