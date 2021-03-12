dd00c0b0c35dbec2a7e7f2a6acd19b1de00901aa
Petar Neshkov <petio567@abv.bg>
2021-03-13 01:00:23 +0200

    added a text processing exercise

---
+++ C#-Fundamentals/Text Processing/Valid Usernames/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+            foreach (var user in usernames)
+            {
+                if (user.Length >= 3 && user.Length < 16)
+                {
+                    bool check = true;
+                    for (int k = 0; k < user.Length; k++)
+                    {
+                        if (!(char.IsLetterOrDigit(user[k]) || user[k] == '_'
+                            || user[k] == '-'))
+                        {
+                            check = false;
+                            break;
+                        }
+                    }
+                    if (check)
+                    {
+                        Console.WriteLine(user);
+                    }
+                }
+
+            }
+        }
+    }
+}
