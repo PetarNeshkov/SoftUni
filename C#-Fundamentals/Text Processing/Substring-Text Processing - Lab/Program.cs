89d3df62ff09a22ace27faf3a0c89de873c63a32
Petar Neshkov <petio567@abv.bg>
2021-03-15 00:47:49 +0200

    added a text processing exercise

---
+++ C#-Fundamentals/Text Processing/Substring-Text Processing - Lab/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Substring_Text_Processing___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = Console.ReadLine().ToLower();
+            string secondString = Console.ReadLine();
+            while (secondString.Contains(firstString))
+            {
+                int index = secondString.IndexOf(firstString);
+                secondString = secondString.Remove(index, firstString.Length);
+            }
+            Console.WriteLine(secondString);
+        }
+    }
+}
