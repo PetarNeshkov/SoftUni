5287ff5ec459f614a08322e9ca65c826eb4cc646
Petar Neshkov <petio567@abv.bg>
2021-03-14 19:44:51 +0200

    added a text processing exercise

---
+++ C#-Fundamentals/Text Processing/Repeat Strings-Text Processing - Lab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Repeat_Strings_Text_Processing___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string []input = Console.ReadLine().Split();
+            StringBuilder concant = new StringBuilder();
+            foreach (var letter in input)
+            {
+                for (int i = 0; i < letter.Length; i++)
+                {
+                    concant.Append(letter);
+                }
+            }
+            Console.WriteLine(concant);
+        }
+    }
+}
