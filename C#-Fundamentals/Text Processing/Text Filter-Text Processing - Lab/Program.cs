575dca34727ef69ba19a73ec2d47a60e408e0d87
Petar Neshkov <petio567@abv.bg>
2021-03-16 22:25:52 +0200

    added a text processing exercise

---
+++ C#-Fundamentals/Text Processing/Text Filter-Text Processing - Lab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Text_Filter_Text_Processing___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+            foreach (var word in words)
+            {
+                string replace = new string('*', word.Length);
+                text = text.Replace(word, replace);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
