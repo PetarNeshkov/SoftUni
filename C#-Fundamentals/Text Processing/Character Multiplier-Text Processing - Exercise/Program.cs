af7d321adc7a2c55dbeca4e34f3a52b1f1bbd7ae
Petar Neshkov <petio567@abv.bg>
2021-03-18 21:35:28 +0200

    added a text processing exercise

---
+++ C#-Fundamentals/Text Processing/Character Multiplier-Text Processing - Exercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Character_Multiplier_Text_Processing___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            string firstString = input[0];
+            string secondString = input[1];
+            int sum = 0;
+            string bigger = "";
+            string shorter = "";
+            if (firstString.Length>secondString.Length)
+            {
+                bigger = firstString;
+                shorter = secondString;
+            }
+            else
+            {
+                bigger = secondString;
+                shorter = firstString;
+            }
+            for (int i = 0; i < shorter.Length; i++)
+            {
+                sum += shorter[i]*bigger[i];
+            }
+            for (int i = shorter.Length; i < bigger.Length; i++)
+            {
+                sum += bigger[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
