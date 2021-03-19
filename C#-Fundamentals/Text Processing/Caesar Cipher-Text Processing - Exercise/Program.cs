4ee14e86384e6facdb76a37b242d04888ac7add7
Petar Neshkov <petio567@abv.bg>
2021-03-20 00:15:19 +0200

    added a text processing exercise

---
+++ C#-Fundamentals/Text Processing/Caesar Cipher-Text Processing - Exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace Caesar_Cipher_Text_Processing___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder newText = new StringBuilder();
+            string text = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++)
+            {
+                newText.Append((char)(text[i] + 3));
+            }
+            Console.WriteLine(newText);
+        }
+    }
+}
