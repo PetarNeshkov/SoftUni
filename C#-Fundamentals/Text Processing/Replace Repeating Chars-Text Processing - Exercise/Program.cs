b2a262e5cdf177765652485cf01d662c5f52a4be
Petar Neshkov <petio567@abv.bg>
2021-03-22 16:20:17 +0200

    added a text processing exercise

---
+++ C#-Fundamentals/Text Processing/Replace Repeating Chars-Text Processing - Exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Replace_Repeating_Chars_Text_Processing___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                char firstLetter = input[i];
+                char secondLetter = input[i + 1];
+                if (firstLetter != secondLetter)
+                {
+                    sb.Append(firstLetter);
+                }
+            }
+            sb.Append(input[input.Length - 1]);
+            Console.WriteLine(sb);
+        }
+    }
+}
