6b10eb1a5c7fe7fbc8b6efe03cd1ba6b0078b16e
Petar Neshkov <petio567@abv.bg>
2021-03-31 09:41:47 +0300

    added a basic syntax exercise

---
+++ C#-Fundamentals/BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/Holidays Between Two Dates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Holidays_Between_Two_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
