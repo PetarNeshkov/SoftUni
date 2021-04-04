a032f4e11fbc8e11f48cc3f3d3ad9bbb8618f174
Petar Neshkov <petio567@abv.bg>
2021-04-04 21:25:44 +0300

    added a basic syntax exercise

---
+++ C#-Fundamentals/BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/Multiplication Table 2.0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int timesToMultiply = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = timesToMultiply; i <= 10; i++)
+            {
+                sum = input * i;
+                Console.WriteLine($"{input} X {i} = {sum}");
+            }
+            if (timesToMultiply > 10)
+            {
+                sum = input * timesToMultiply;
+                Console.WriteLine($"{input} X {timesToMultiply} = {sum}");
+            }
+        }
+    }
+}
