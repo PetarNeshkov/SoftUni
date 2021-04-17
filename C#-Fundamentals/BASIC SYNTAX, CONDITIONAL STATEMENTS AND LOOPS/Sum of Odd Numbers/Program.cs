99ada90b292fb63255164c4a93aa77015313d760
Petar Neshkov <petio567@abv.bg>
2021-04-17 15:23:35 +0300

    added a basic syntax exercise

---
+++ C#-Fundamentals/BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/Sum of Odd Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int check = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    sum += i;
+                    check++;
+                    if (check == numbers)
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
