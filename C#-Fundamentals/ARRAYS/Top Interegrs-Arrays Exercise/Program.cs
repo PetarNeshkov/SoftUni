1163b3c1d6ca9ad4be2e8574b8753b089ce97710
Petar Neshkov <petio567@abv.bg>
2021-05-11 00:00:24 +0300

    added an array exercise

---
+++ C#-Fundamentals/ARRAYS/Top Interegrs-Arrays Exercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Top_Interegrs_Arrays_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+            bool isBigger = true;
+                int currentInt = array[i];
+                for (int k = i+1; k < array.Length; k++)
+                {
+                    if (currentInt <= array[k])
+                    {
+                        isBigger = false;
+                        break;
+                    }
+                }
+                if (isBigger==true)
+                {
+                    Console.Write(currentInt+ " ");
+                }
+            }
+        }
+    }
+}
