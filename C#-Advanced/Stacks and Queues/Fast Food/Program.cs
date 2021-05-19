63124ea74e6021dfaf281f9b9419745e2446c2f4
Petar Neshkov <petio567@abv.bg>
2021-05-19 23:52:18 +0300

    added stacks and queues exercise

---
+++ C#-Advanced/Stacks and Queues/Fast Food/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mainFoodQuantity = int.Parse(Console.ReadLine());
+            int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(orders);
+            Console.WriteLine(orders.Max());
+            while (queue.Count>0)
+            {
+                int food = queue.Peek();
+                if (mainFoodQuantity>=food)
+                {
+                    mainFoodQuantity -= food;
+                    queue.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (queue.Count>0)
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ",queue)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
