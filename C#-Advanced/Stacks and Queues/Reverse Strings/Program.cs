2164406957e4d636e6d87f3cb5f3ee9dcb8994f7
Petar Neshkov <petio567@abv.bg>
2021-05-25 00:00:32 +0300

    added stacks and queues exercise

---
+++ C#-Advanced/Stacks and Queues/Reverse Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            foreach (var letter in input)
+            {
+                stack.Push(letter);
+            }
+            while (stack.Count>0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
