5051de4135a455ccb24bb33841ef5251b3e45cf3
Petar Neshkov <petio567@abv.bg>
2021-05-26 21:13:59 +0300

    added stack and queue exercise

---
+++ C#-Advanced/Stacks and Queues/Simple Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            Stack<string> stack = new Stack<string>(input);
+            while (stack.Count>1)
+            {
+                int firstNum = int.Parse(stack.Pop());
+                string sign = stack.Pop();
+                int secondNum = int.Parse(stack.Pop());
+                if (sign=="+")
+                {
+                    stack.Push((firstNum + secondNum).ToString());
+                }
+                else if (sign=="-")
+                {
+                    stack.Push((firstNum - secondNum).ToString());
+                }
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
