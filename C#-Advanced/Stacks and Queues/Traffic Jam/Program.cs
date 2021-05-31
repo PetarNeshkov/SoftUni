ed3a55938f0ed8c261b884c202355b0d9e84f5fd
Petar Neshkov <petio567@abv.bg>
2021-05-31 23:40:42 +0300

    added stack and queue exercise

---
+++ C#-Advanced/Stacks and Queues/Traffic Jam/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>();
+            string input = "";
+            int passedCars = 0;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input == "green")
+                {
+                    if (count > queue.Count)
+                    {
+                        count = queue.Count;
+                    }
+                        for (int i = 0; i < count; i++)
+                        {
+                            Console.WriteLine($"{queue.Dequeue()} passed!");
+                            passedCars++;
+                        }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+            }
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
+        }
+    }
+}
