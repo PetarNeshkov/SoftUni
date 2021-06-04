d35b48d6932d853680ea6e7d973ae2d87b139bbd
Petar Neshkov <petio567@abv.bg>
2021-06-04 23:39:11 +0300

    added a multidimensional array exercise

---
+++ C#-Advanced/MULTIDIMENSIONAL ARRAYS/Bombs/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+            fillMatrix(matrix);
+            string[] coordinates = Console.ReadLine().Split();
+            foreach (var Pair in coordinates)
+            {
+
+                int[] currentBombCoordinates = Pair
+                    .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                int currRow = currentBombCoordinates[0];
+                int currCol = currentBombCoordinates[1];
+
+                int currBomb = matrix[currRow, currCol];
+                for (int row = currRow - 1; row <= currRow + 1; row++)
+                {
+                    for (int col = currCol - 1; col <= currCol + 1; col++)
+                    {
+                        if (row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1))
+                        {
+                            if (matrix[row, col] > 0 && currBomb > 0)
+                            {
+                                matrix[row, col] -= currBomb;
+                            }
+                        }
+
+                    }
+                }
+            }
+            int sum = 0;
+            int aliveCells = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        aliveCells++;
+                        sum += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {aliveCells}");
+            Console.WriteLine($"Sum: {sum}");
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        private static void fillMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+        }
+    }
+}
