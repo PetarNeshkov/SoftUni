a8030eb331e1982fb9d361b36d695431275dc89a
Petar Neshkov <petio567@abv.bg>
2021-06-21 00:01:45 +0300

    added a multidimensional array exercise

---
+++ C#-Advanced/MULTIDIMENSIONAL ARRAYS/Symbol in Matrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[size, size];
+            bool check = false;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] text = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = text[col];
+                }
+            }
+            string element = Console.ReadLine();
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col].ToString().Contains(element))
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        check = true;
+                        return;
+                    }
+                }
+            }
+            if (!check)
+            {
+                Console.WriteLine($"{element} does not occur in the matrix");
+            }
+        }
+    }
+}
