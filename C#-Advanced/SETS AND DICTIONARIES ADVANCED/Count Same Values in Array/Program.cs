389b3f8c4bd42fc5ec4efc7ecf31eb2f0f8d6268
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-06-27 00:00:58 +0300

    added a set/dictionary exercise

---
+++ C#-Advanced/SETS AND DICTIONARIES ADVANCED/Count Same Values in Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double, int> dc = new Dictionary<double, int>();
+            double[] input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            for (int i = 0; i < input.Length; i++)
+            {
+                double currDigit = input[i];
+                if (!dc.ContainsKey(currDigit))
+                {
+                    dc.Add(currDigit, 1);
+                }
+                else
+                {
+                    dc[currDigit]++;
+                }
+            }
+            foreach (var item in dc)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }     
+        }
+    }
+}
