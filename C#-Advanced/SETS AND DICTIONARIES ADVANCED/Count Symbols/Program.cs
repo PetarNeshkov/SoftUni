0ff2545dccd9f3bff7cb332b47676701552f5eb4
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-06-28 12:05:40 +0300

    added a set/dictionary exercise

---
+++ C#-Advanced/SETS AND DICTIONARIES ADVANCED/Count Symbols/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> dc = new SortedDictionary<char, int>();
+            string input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currChar = input[i];
+                if (!dc.ContainsKey(currChar))
+                {
+                    dc.Add(currChar,0);
+                }
+                dc[currChar]++;
+            }
+            foreach (var item in dc)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
