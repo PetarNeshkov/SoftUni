a3a600b5c3b5a0562e2449a65dddbc5d2370f64c
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-06-30 18:17:37 +0300

    added a set/dictionary exercise

---
+++ C#-Advanced/SETS AND DICTIONARIES ADVANCED/Even Times/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> dc = new Dictionary<int, int>();
+            int counter = int.Parse(Console.ReadLine());
+            for (int i = 0; i < counter; i++)
+            {
+                int digit = int.Parse(Console.ReadLine());
+                if (!dc.ContainsKey(digit))
+                {
+                    dc.Add(digit, 0);
+                }
+                dc[digit]++;
+            }
+           int num= dc.Where(x => x.Value % 2 == 0).FirstOrDefault().Key;
+            Console.WriteLine(num);
+        }
+    }
+}
