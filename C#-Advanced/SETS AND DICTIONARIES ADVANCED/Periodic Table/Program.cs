f0f9dbebee8491b09e51cf56dc449019c2d788fe
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-07-02 01:03:29 +0300

    added a set/dictionary exercise

---
+++ C#-Advanced/SETS AND DICTIONARIES ADVANCED/Periodic Table/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+            SortedSet<string> hash = new SortedSet<string>();
+            for (int i = 0; i < counter; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                for (int k = 0; k < input.Length; k++)
+                {
+                    string curString = input[k];
+                    hash.Add(curString);
+                }
+            }
+            Console.WriteLine(String.Join(" ",hash));
+        }
+    }
+}
