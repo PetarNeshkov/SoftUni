9b2f91e56263e227622faf22b0ec57b219ca1cf6
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-07-01 23:14:52 +0300

    added a set/dictionary exercise

---
+++ C#-Advanced/SETS AND DICTIONARIES ADVANCED/Parking Lot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            HashSet<string> hash = new HashSet<string>();
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] information = input.Split(",");
+                string action = information[0];
+                string carNumber = information[1];
+                if (action == "IN")
+                {
+                    if (!hash.Contains(carNumber))
+                    {
+                        hash.Add(carNumber);
+                    }
+                }
+                else if (action == "OUT")
+                {
+                    if (hash.Contains(carNumber))
+                    {
+                        hash.Remove(carNumber);
+                    }
+                }
+            }
+            if (hash.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var item in hash)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
