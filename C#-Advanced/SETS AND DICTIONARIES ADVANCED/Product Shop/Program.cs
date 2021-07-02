be57406f6dc91199db6da31d5bc67e44f970d841
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-07-03 00:43:16 +0300

    added a set/dictionary exercise

---
+++ C#-Advanced/SETS AND DICTIONARIES ADVANCED/Product Shop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Product_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            SortedDictionary<string, Dictionary<string, double>> shops = new SortedDictionary<string, Dictionary<string, double>>();
+            while ((input=Console.ReadLine())!="Revision")
+            {
+                string[] information = input.Split(", ");
+                string shop = information[0];
+                string product = information[1];
+                double price = double.Parse(information[2]);
+                if (!shops.ContainsKey(shop))
+                {
+                    shops.Add(shop, new Dictionary<string, double>());
+                }
+                if (!shops[shop].ContainsKey(product))
+                {
+                    shops[shop].Add(product, price);
+                }
+            }
+            foreach (var item in shops)
+            {
+                Console.WriteLine($"{item.Key}->");
+                foreach (var product in item.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
