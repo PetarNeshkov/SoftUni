ef0a26d803407f557e55d78af20cc5646885f726
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-07-14 10:15:06 +0300

    added a set/dictionary exercise

---
+++ C#-Advanced/SETS AND DICTIONARIES ADVANCED/Record Unique Names/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+            HashSet<string> hash = new HashSet<string>();
+            for (int i = 0; i < counter; i++)
+            {
+                string name = Console.ReadLine();
+                if (!hash.Contains(name))
+                {
+                    hash.Add(name);
+                }
+            }
+            foreach (var item in hash)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
