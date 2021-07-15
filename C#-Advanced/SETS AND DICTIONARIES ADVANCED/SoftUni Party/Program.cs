3996d37c56eba928de2186e9ff46beeb382f3a40
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-07-16 00:37:21 +0300

    added a set/dictionary exercise

---
+++ C#-Advanced/SETS AND DICTIONARIES ADVANCED/SoftUni Party/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            string input = "";
+            while ((input=Console.ReadLine())!="PARTY")
+            {
+                set.Add(input);
+            }
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (set.Contains(input))
+                {
+                    set.Remove(input);
+                }
+            }
+            Console.WriteLine(set.Count);
+            foreach (var item in set)
+            {
+                char[] ch = item.ToCharArray();
+                if (char.IsDigit(ch[0]))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            foreach (var item in set)
+            {
+                char[] ch = item.ToCharArray();
+                if (char.IsLetter(ch[0]))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
