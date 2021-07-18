778769e55ec6476f8294a54862829d8bf57cb050
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-07-18 21:48:55 +0300

    added a set/dictionary exercise

---
+++ C#-Advanced/SETS AND DICTIONARIES ADVANCED/Unique Usernames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+            HashSet<string> hash = new HashSet<string>();
+            for (int i = 0; i < counter; i++)
+            {
+                string name = Console.ReadLine();
+                hash.Add(name);
+            }
+            foreach (var item in hash)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
