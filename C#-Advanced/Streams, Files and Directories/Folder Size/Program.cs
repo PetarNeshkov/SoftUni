6302f7b99d2b316a87d4bfec3861eb8dc1d73027
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-07-23 13:48:09 +0300

    added a file/stream exercise

---
+++ C#-Advanced/Streams, Files and Directories/Folder Size/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPatch = Console.ReadLine();
+            //D:\SoftUni-Fund\Advanced\Streams, Files and Directories\Slice a File
+            string[] files = Directory.GetFiles(directoryPatch);
+            double sum = 0;
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo info = new FileInfo(files[i]);
+                Console.WriteLine($"{info.FullName}  --> {info.Length} bytes");
+                sum += info.Length;
+            }
+            sum = sum / 1024 / 1024;
+
+            File.WriteAllText("Output.txt", sum.ToString());
+        }
+    }
+}
