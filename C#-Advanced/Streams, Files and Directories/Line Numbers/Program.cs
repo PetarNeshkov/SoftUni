2099ea10d3325b09e6dece5fda134f191dfd6418
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-07-24 10:12:46 +0300

    added a file/set exercise

---
+++ C#-Advanced/Streams, Files and Directories/Line Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                using (StreamWriter writer=new StreamWriter("../../../output.txt"))
+                {
+                    string line = reader.ReadLine();
+                    int index = 1;
+                    while (line!=null)
+                    {
+                        writer.WriteLine($"{index}. {line}line");
+                        Console.WriteLine($"{index}. {line}line");
+                        index++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
