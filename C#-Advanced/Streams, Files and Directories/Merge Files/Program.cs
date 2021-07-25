3c0d4254ba22377a82cca0668963b9dbfb66df8a
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-07-26 00:22:43 +0300

    added a file/set exercise

---
+++ C#-Advanced/Streams, Files and Directories/Merge Files/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Merge_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstFileName = "../../../FileOne.txt";
+            string secondFileName = "../../../FileTwo.txt";
+
+          
+            var mergedList = new List<string>();
+
+            var firstFileLines = File.ReadAllLines(firstFileName);
+            var secondFileLines = File.ReadAllLines(secondFileName);
+
+            foreach (var line in firstFileLines)
+            {
+                mergedList.Add(line);
+            }
+
+            foreach (var line in secondFileLines)
+            {
+                mergedList.Add(line);
+            }
+
+            string outputFilePath = "../../../Output.txt";
+
+            File.WriteAllLines(outputFilePath, mergedList.OrderBy(x => x));
+        }
+    }
+}
