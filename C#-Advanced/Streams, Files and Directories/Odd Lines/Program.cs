3cdc79497e54fb3f35509d6cb616d0ce56f58ea4
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-07-27 22:22:23 +0300

    added a file/set exercise

---
+++ C#-Advanced/Streams, Files and Directories/Odd Lines/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+            {
+                using (StreamReader reader = new StreamReader("../../../input.txt"))
+                {
+                    string line = reader.ReadLine();
+                    int index = 0;
+                    while (line != null)
+                    {
+                        if (index % 2 == 1)
+                        {
+                            Console.WriteLine(line);
+                            writer.WriteLine(line);
+                        }
+                        index++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
