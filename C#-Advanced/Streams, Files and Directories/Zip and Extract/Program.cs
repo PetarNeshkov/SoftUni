cc3185aad3ad0e9646d12c04c7930c628245e92e
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-07-31 00:11:54 +0300

    added a file/set exercise

---
+++ C#-Advanced/Streams, Files and Directories/Zip and Extract/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace Zip_and_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory("../../../ZipDemo1","../../../ZipDemo2/ZipDemo2.zip");
+            ZipFile.ExtractToDirectory("../../../ZipDemo2/ZipDemo2.zip","../../../Extract");
+        }
+    }
+}
