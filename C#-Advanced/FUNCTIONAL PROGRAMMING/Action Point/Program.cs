48b1e88fd6faa4a334c4c5f075cfa449e1034404
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-08-15 19:03:55 +0300

    added a functional programming exercise

---
+++ C#-Advanced/FUNCTIONAL PROGRAMMING/Action Point/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            Action<string[]> print = x => Console.WriteLine(String.Join(Environment.NewLine,x));
+            print(names);
+        }
+    }
+}
