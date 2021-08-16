cc86dfb7b0b918bef652b9b961e3d3f5713780e1
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-08-16 18:52:21 +0300

    added a functional programming exercise

---
+++ C#-Advanced/FUNCTIONAL PROGRAMMING/Add VAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] values = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x => x * 1.20)
+                .ToArray();
+            foreach (var item in values)
+            {
+                Console.WriteLine($"{item:F2}");
+            }
+        }
+    }
+}
