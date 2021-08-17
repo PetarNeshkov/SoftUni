865675d00e9f429e78509b9c9636ccbe1ea90448
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-08-18 01:46:28 +0300

    added a functional programming exercise

---
+++ C#-Advanced/FUNCTIONAL PROGRAMMING/Count Uppercase Words/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> checker = x => x[0] == x.ToUpper()[0];
+            string[] text = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Where(checker).ToArray();
+            foreach (var item in text)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
