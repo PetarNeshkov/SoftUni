8cee877255723cf2d2521d296328c8f4c75c835b
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-08-19 10:37:31 +0300

    added a functional programming exercise

---
+++ C#-Advanced/FUNCTIONAL PROGRAMMING/Custom Comparator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Array.Sort(numbers, (x, y) =>
+             {
+                 int sort = -1;
+                 if (x % 2 == 0 && y % 2 != 0)
+                 {
+                     sort = -1;
+                 }
+                 else if (x % 2 != 0 && y % 2 == 0)
+                 {
+                     sort = 1;
+                 }
+                 else
+                 {
+                     sort = x - y;
+                     //y.CompareTo(x);
+                 }
+                 return sort;
+             });
+            //Array.Sort(numbers, (x, y) => (x % 2 == 0 && y % 2 != 0) ? -1 :
+            //(x % 2 != 0 && y % 2 == 0) ? 1
+            //: x.CompareTo(y));
+
+            Console.WriteLine(String.Join(" ",numbers));
+        }
+    }
+}
