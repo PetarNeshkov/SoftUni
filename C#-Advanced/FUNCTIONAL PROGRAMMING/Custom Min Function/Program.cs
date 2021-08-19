7ff9b80387fdc145c4aa7bc80fb0da2c5dd56a74
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-08-20 00:53:32 +0300

    added a functional programming exercise

---
+++ C#-Advanced/FUNCTIONAL PROGRAMMING/Custom Min Function/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int[], int> check = numbers =>
+             {
+                 int min = int.MaxValue;
+                 foreach (var number in numbers)
+                 {
+                     if (min > number)
+                     {
+                         min = number;
+                     }
+                 }
+                 return min;
+             };
+            Console.WriteLine(check(numbers));
+        }
+    }
+
+}
