be0aa23ec924b6049c360704ff5ec0d8d38ce02e
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-08-23 00:02:57 +0300

    added a functional programming exercise

---
+++ C#-Advanced/FUNCTIONAL PROGRAMMING/Knights of Honor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            names = names.Select(x => $"Sir {x}").ToArray();
+            Action<string[]> print = x => Console.WriteLine($"{String.Join(Environment.NewLine, names)}");
+            print(names);
+        }
+    }
+}
