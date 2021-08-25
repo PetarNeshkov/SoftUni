6679998bbac4407c2137124d9a265f99d2c36b2c
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-08-26 00:19:22 +0300

    added a functional programming exercise

---
+++ C#-Advanced/FUNCTIONAL PROGRAMMING/Predicate For Names/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght =int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+            names = names.Where(x => x.Length <= lenght).ToArray();
+            Console.WriteLine($"{String.Join(Environment.NewLine,names)}");
+        }
+    }
+}
