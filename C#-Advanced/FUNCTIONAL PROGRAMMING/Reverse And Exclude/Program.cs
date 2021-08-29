d4bb9b16ff08b60d454ff2404131f35713fdef3e
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-08-29 16:59:08 +0300

    added a functional programming exercise

---
+++ C#-Advanced/FUNCTIONAL PROGRAMMING/Reverse And Exclude/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int number =int.Parse(Console.ReadLine());
+            numbers = numbers.Where(x => x % number != 0)
+                .Reverse()
+                .ToArray();
+            Console.WriteLine(String.Join(" ",numbers));
+
+        }
+    }
+}
