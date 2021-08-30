56f2c7793c0d1b62737501fadd4187d7dda9e394
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-08-30 10:59:17 +0300

    added a functional programming exercise

---
+++ C#-Advanced/FUNCTIONAL PROGRAMMING/Sort Even Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).OrderBy(x => x)
+                .Where(x=>x%2==0).ToArray();
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+    }
+}
