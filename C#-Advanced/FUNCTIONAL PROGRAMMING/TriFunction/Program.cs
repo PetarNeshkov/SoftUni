a52642ef2bbfc7b4fdcd01c95f338e2b8480da09
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-09-01 14:58:42 +0300

    added a functional programming exercise

---
+++ C#-Advanced/FUNCTIONAL PROGRAMMING/TriFunction/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+            Console.WriteLine(names.FirstOrDefault(x => x.ToCharArray()
+            .Select(y => (int)y)
+            .Sum()>=n));
+        }
+    }
+}
