c5a7f25f4d441f3e73c6b2c193dedcd64d9209a6
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-09-02 01:49:43 +0300

    added a class exercise

---
+++ C#-Advanced/Defining Classes/Car/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "brum";
+            car.Year = 5360;
+            car.Model = "BMV";
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
