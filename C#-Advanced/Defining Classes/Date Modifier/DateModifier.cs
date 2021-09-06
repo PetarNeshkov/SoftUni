1b74ed0a82ecf13650bc97400b817b0956fe66da
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-09-07 01:35:14 +0300

    added a class exercise

---
+++ C#-Advanced/Defining Classes/Date Modifier/DateModifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifier
+{
+    class DateModifier
+    {
+        public int GetDayDifference(string startDateString,string endDateString)
+        {
+            DateTime startDate = DateTime.Parse(startDateString);
+            DateTime endDate = DateTime.Parse(endDateString);
+            int result = (int)(endDate - startDate).TotalDays;
+            return result;
+        }
+    }
+}
---
+++ C#-Advanced/Defining Classes/Date Modifier/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateModifier
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string startDate = Console.ReadLine();
+            string endDate = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier();
+            int result = dateModifier.GetDayDifference(startDate, endDate);
+            Console.WriteLine(Math.Abs(result));
+        }
+    }
+}
