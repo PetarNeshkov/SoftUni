52d96f428edc74e3394a11902a6f1750d3dfd80c
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2021-09-08 17:49:55 +0300

    added a class exercise

---
+++ C#-Advanced/Defining Classes/Define a Class Person/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+  public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person firstPeson = new Person("Pesho", 20);
+            Person secondPerson = new Person("Gosho", 18);
+            Person thirdPerson = new Person("Stamat", 43);
+        }
+    }
+}
