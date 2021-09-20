34bdaf63b55b5f7e6ce760985347dbde18cc2a6a
Petar <petio567@abv.bg>
2021-09-20 17:59:55 +0300

    added a classs exercise

---
+++ C#-Advanced/Defining Classes/Pokemon Trainer/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pokemon
+{
+  public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            Dictionary<string, Trainer> trainers = new Dictionary<string, Trainer>();
+            while ((input = Console.ReadLine()) != "Tournament")
+            {
+                string[] split = input.Split();
+                string trainneName = split[0];
+                string pokemonName = split[1];
+                string pokemonElement = split[2];
+                int pokemonHealth = int.Parse(split[3]);
+                Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+                if (!trainers.ContainsKey(trainneName))
+                {
+                    trainers.Add(trainneName, new Trainer(trainneName));
+                }
+                trainers[trainneName].Add(pokemon);
+            }
+            while ((input = Console.ReadLine()) != "End")
+            {
+                foreach (var trainer in trainers.Values)
+                {
+                    trainer.Check(input);
+                }
+            
+            }
+            List<Trainer> result = trainers.Values
+               .OrderByDescending(x => x.NumberOfBadges)
+               .ToList();
+            foreach (var trainer in result)
+            {
+                Console.WriteLine(trainer);
+            }
+        }
+    }
+}
---
+++ C#-Advanced/Defining Classes/Pokemon Trainer/Trainer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pokemon
+{
+   public class Trainer
+    {
+        private List<Pokemon> pokemons;
+        public Trainer(string name)
+        {
+            Name = name;
+            NumberOfBadges = 0;
+            pokemons = new List<Pokemon>();
+        }
+        public string Name { get; set; }
+        public int NumberOfBadges { get; set; }
+        public void Add(Pokemon pokemon)
+        {
+            pokemons.Add(pokemon);
+        }
+         public void Check(string element)
+        {
+            bool hasPokemon = false;
+            foreach (var pokemon in pokemons)
+            {
+                if (pokemon.Element==element)
+                {
+                    NumberOfBadges++;
+                    hasPokemon = true;
+                    break;
+                }
+            }
+            if (!hasPokemon)
+            {
+                foreach (var pokemon in pokemons)
+                {
+                    pokemon.Health -= 10;
+                }
+            }
+            pokemons = pokemons.Where(x => x.Health > 0).ToList();
+        }
+        public override string ToString()
+        {
+            return $"{Name} {NumberOfBadges} {pokemons.Count}";
+        }
+    }
+
+}
