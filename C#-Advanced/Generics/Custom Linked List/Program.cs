ad8e56f0df0764f49c42cbe1f880842d9d24df0d
Petar <petio567@abv.bg>
2021-09-29 00:44:19 +0300

    added a generic exercise

---
+++ C#-Advanced/Generics/Custom Linked List/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace CustomLinkedList
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var customLinkedList = new CustomLinkedList<int>();
+        }
+    }
+}
