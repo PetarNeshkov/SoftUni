2e788dbdcbc07d869d7548914f1c0bb84f599cfb
Petar <petio567@abv.bg>
2021-10-03 00:33:40 +0300

    added a generic exercise

---
+++ C#-Advanced/Generics/Generic Count Method Double/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Count_Method_Double
+{
+   public class Box<T> where T:IComparable
+    {
+        private List<T> information;
+        public Box()
+        {
+            information = new List<T>();
+        }
+        public void Add(T input)
+        {
+            information.Add(input);
+        }
+        public int Check(T number)
+        {
+            int count = 0;
+            foreach (var currNumber in information)
+            {
+                if (currNumber.CompareTo(number)>0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ C#-Advanced/Generics/Generic Count Method Double/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Generic_Count_Method_Double
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =int.Parse(Console.ReadLine());
+            Box<double> box = new Box<double>();
+            for (int i = 0; i < n; i++)
+            {
+                double number =double.Parse(Console.ReadLine());
+                box.Add(number);
+            }
+            double numberToCheck =double.Parse(Console.ReadLine());
+            int count=box.Check(numberToCheck);
+            Console.WriteLine(count);
+        }
+    }
+}
