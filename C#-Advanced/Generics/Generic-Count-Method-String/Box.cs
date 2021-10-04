c404240f7f7b05e6f78aa1d8efdb7aac5d4238a1
Petar <petio567@abv.bg>
2021-10-05 00:11:58 +0300

    Added a generic exercise

---
+++ C#-Advanced/Generics/Generic-Count-Method-String/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Count_Method_String
+{
+    public class Box<T> where T : IComparable
+    {
+        private List<T> information;
+        public Box()
+        {
+            information = new List<T>();
+        }
+        public void Add(T value)
+        {
+            information.Add(value);
+        }
+        public int GetCountOfEl(T value)
+        {
+            int count = 0;
+            foreach (var item in information)
+            {
+                if (value.CompareTo(item)<0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ C#-Advanced/Generics/Generic-Count-Method-String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Generic_Count_Method_String
+{
+  public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                box.Add(input);
+            }
+            string comparingEl = Console.ReadLine();
+            int count=box.GetCountOfEl(comparingEl);
+            Console.WriteLine(count);
+        }
+    }
+}
