2f6da070d260dfac79673b6296485a1c6b2920fb
Petar <petio567@abv.bg>
2021-10-06 00:13:37 +0300

    added a generic exercise

---
+++ C#-Advanced/Generics/Generic-Swap-Method-Integers/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Swap_Method_Integers
+{
+   public class Box<T>
+    {
+        private List<T> information;
+        public Box()
+        {
+            information = new List<T>();
+        }
+        public void Add(T number)
+        {
+            information.Add(number);
+        }
+        public void Swap(int firstIndex,int secondIndex)
+        {
+            T temp = information[firstIndex];
+            information[firstIndex] = information[secondIndex];
+            information[secondIndex] = temp;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in information)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ C#-Advanced/Generics/Generic-Swap-Method-Integers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Generic_Swap_Method_Integers
+{
+  public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =int.Parse(Console.ReadLine());
+            Box<int> box = new Box<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int currNumber =int.Parse(Console.ReadLine());
+                box.Add(currNumber);
+            }
+            int[] split = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int firstNumber = split[0];
+            int secondNumber = split[1];
+            box.Swap(firstNumber, secondNumber);
+            Console.WriteLine(box);
+        }
+    }
+}
