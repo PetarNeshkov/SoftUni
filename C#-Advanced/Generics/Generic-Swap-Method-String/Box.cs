9880162f4f6bd0c109b4936bb78972e821b5fccf
Petar <petio567@abv.bg>
2021-10-07 00:49:50 +0300

    added a generic exercise

---
+++ C#-Advanced/Generics/Generic-Swap-Method-String/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Swap_Method_String
+{
+    public class Box<T>
+    {
+        private List<T> information;
+        public Box()
+        {
+            information = new List<T>();
+        }
+        public void Add(T input)
+        {
+            information.Add(input);
+        }
+        public void Swap(int firstIndex,int secondIndex)
+        {
+            T temp = information[firstIndex];
+            information[firstIndex] = information[secondIndex];
+            information[secondIndex] = temp;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in information)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ C#-Advanced/Generics/Generic-Swap-Method-String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generic_Swap_Method_String
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                box.Add(input);
+            }
+            int[] split = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int firstIndex = split[0];
+            int secondIndex = split[1];
+            box.Swap(firstIndex, secondIndex);
+            Console.WriteLine(box);
+        }
+    }
+}
