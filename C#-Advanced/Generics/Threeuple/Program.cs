2e43a0f6c02c81371998d9d04c37425fff580e44
Petar <petio567@abv.bg>
2021-10-08 11:33:53 +0300

    added a generic exercise

---
+++ C#-Advanced/Generics/Threeuple/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Threeuple
+{
+  public  class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstEl = Console.ReadLine().Split();
+            string fullName = $"{firstEl[0]} {firstEl[1]}";
+            string adress = firstEl[2];
+            string town = firstEl[3];
+            Threeuple<string, string, string> Tuple1 = new Threeuple<string, string, string>(fullName,adress,town);
+
+            string[] secondEl = Console.ReadLine().Split();
+            string name = secondEl[0];
+            int liters = int.Parse(secondEl[1]);
+            string condition = secondEl[2];
+            bool check = Check(condition);
+            Threeuple<string, int, bool> Tuple2 = new Threeuple<string, int, bool>(name, liters, check);
+
+            string[] thirdEl = Console.ReadLine().Split();
+            string name2 = thirdEl[0];
+            double balance = double.Parse(thirdEl[1]);
+            string bankName = thirdEl[2];
+            Threeuple<string, double, string> Tuple3 = new Threeuple<string, double, string>(name2, balance, bankName);
+
+            Console.WriteLine(Tuple1);
+            Console.WriteLine(Tuple2);
+            Console.WriteLine(Tuple3);
+        }
+        public static bool Check(string condition)
+        {
+            if (condition=="drunk")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ C#-Advanced/Generics/Threeuple/Threeuple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threeuple
+{
+   public class Threeuple<TFirst,TSecond,TThird>
+    {
+        TFirst item1 { get; set; }
+        TSecond item2 { get; set; }
+        TThird item3 { get; set; }
+        public Threeuple(TFirst firstItem,TSecond secondItem,TThird thirdItem)
+        {
+            item1 = firstItem;
+            item2 = secondItem;
+            item3 = thirdItem;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{item1} -> {item2} -> {item3}");
+            return sb.ToString();
+        }
+    }
+}
