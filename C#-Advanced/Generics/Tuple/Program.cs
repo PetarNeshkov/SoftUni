5725e21b84d280f5abe09270ffd85309e0733f58
Petar <petio567@abv.bg>
2021-10-09 01:12:36 +0300

    added a generic exercise

---
+++ C#-Advanced/Generics/Tuple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tuple
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split();
+            string FullName = $"{firstInput[0]} {firstInput[1]}";
+            string adress = firstInput[2];
+            Tuple<string,string> Tuple1 = new Tuple<string,string>(FullName,adress);
+
+            string[] secondInput = Console.ReadLine().Split();
+            string name = secondInput[0];
+            int litersOfBeer = int.Parse(secondInput[1]);
+            Tuple<string, int> Tuple2 = new Tuple<string, int>(name, litersOfBeer);
+
+            string[] thirdInput = Console.ReadLine().Split();
+            int intNum = int.Parse(thirdInput[0]);
+            double doubleNum = double.Parse(thirdInput[1]);
+            Tuple<int, double> Tuple3 = new Tuple<int, double>(intNum, doubleNum);
+
+            Console.WriteLine(Tuple1);
+            Console.WriteLine(Tuple2);
+            Console.WriteLine(Tuple3);
+        }
+    }
+}
---
+++ C#-Advanced/Generics/Tuple/Tuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+   public class Tuple<TFirst,TSecond>
+    {
+       private TFirst Item1 { get; set; }
+        private TSecond Item2 { get; set; }
+        public Tuple(TFirst firstItem,TSecond secondItem)
+        {
+            Item1 = firstItem;
+            Item2 = secondItem;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{Item1} -> {Item2}");
+            return sb.ToString();
+        }
+    }
+}
