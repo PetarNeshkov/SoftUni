4ee3e83d0ed562d0655aff7cd2d6f9aaaf57cc28
Petar <petio567@abv.bg>
2021-10-10 00:01:34 +0300

    added a Iterators and Comparators exercise

---
+++ C#-Advanced/Iterators and Comparators/Book Comparator/Library.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        public Library(params Book[] books)
+        {
+            Books = new SortedSet<Book>(books,new BookComparator());
+        }
+        public SortedSet<Book> Books { get; set; }
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return Books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
