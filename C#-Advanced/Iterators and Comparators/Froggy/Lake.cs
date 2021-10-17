901889d86a12f7000ecf2d47084fa76b48402c32
Petar <petio567@abv.bg>
2021-10-17 10:18:02 +0300

    added an Iterators and Comparators exercise

---
+++ C#-Advanced/Iterators and Comparators/Froggy/Lake.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        public Lake(int[] information)
+        {
+            Information = information;
+        }
+        int[] Information;
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < Information.Length; i+=2)
+            {
+                yield return Information[i];
+            }
+            for (int i = Information.Length - 1; i > 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return Information[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C#-Advanced/Iterators and Comparators/Froggy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stones = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            Lake lake = new Lake(stones);
+
+            List<int> path = new List<int>();
+        
+            Console.WriteLine(String.Join(", ",lake));
+        }
+    }
+}
