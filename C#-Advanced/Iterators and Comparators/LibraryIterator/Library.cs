4086805ef1d944972ab57a676fff0f74d9a9e5c8
Petar <petio567@abv.bg>
2021-10-19 00:08:59 +0300

    added an Iterators and Comparators exercise

---
+++ C#-Advanced/Iterators and Comparators/LibraryIterator/Library.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+   public class Library:IEnumerable<Book>
+    {
+        public Library(params Book[] books)
+        {
+            Books = books.ToList();
+        }
+        public List<Book> Books { get; set; }
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(Books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            public LibraryIterator(List<Book> books)
+            {
+                Books = books;
+            }
+            private int index = -1;
+            public List<Book> Books { get; set; }
+            public Book Current => Books[index];
+
+            object IEnumerator.Current => Books[index];
+
+            public void Dispose()
+            {
+                
+            }
+
+            public bool MoveNext()
+            {
+                return ++index < Books.Count;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+        }
+    }
+}
