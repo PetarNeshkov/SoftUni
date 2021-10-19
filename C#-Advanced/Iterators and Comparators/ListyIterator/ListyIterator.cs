2e5551a94de4852e1fab3be1b02314fa6b041ddc
Petar <petio567@abv.bg>
2021-10-20 01:12:33 +0300

    added an Iterators and Comparators exercise

---
+++ C#-Advanced/Iterators and Comparators/ListyIterator/ListyIterator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+   public class ListyIterator<T>
+    {
+        private List<T> Information;
+        private int index=0;
+        public ListyIterator(params T[] elements)
+        {
+            this.Information = new List<T>();
+
+        }
+        public bool Move()
+        {
+            if (index+1<Information.Count)
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+        public bool HasNext()
+        {
+            if (index+1<Information.Count)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void Print()
+        {
+            if (Information.Count==0)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            else if(index>=0 && index < Information.Count)
+            {
+                Console.WriteLine(Information[index]);
+            }
+        }
+        public void Add(T item)
+        {
+            Information.Add(item);
+        }
+
+    }
+}
---
+++ C#-Advanced/Iterators and Comparators/ListyIterator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> listyIterator = new ListyIterator<string>();
+            string input = "";
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (input.Contains("Create"))
+                {
+                    string[] createCommand = input
+                        .Split(new string[] { "Create", " " }, StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+                    for (int i = 0; i < createCommand.Length; i++)
+                    {
+                        listyIterator.Add(createCommand[i]);
+                    }
+                }
+                else if (input=="Move")
+                {
+                    Console.WriteLine(listyIterator.Move()); 
+                }
+                else if (input=="Print")
+                {
+                    listyIterator.Print();
+                }
+                else if (input=="HasNext")
+                {
+                    Console.WriteLine(listyIterator.HasNext()); 
+                }
+            }
+        }
+    }
+}
