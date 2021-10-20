9b6b2b30dddc9fec23275d4306bd18fff94230c6
Petar <petio567@abv.bg>
2021-10-21 00:29:25 +0300

    added an Iterators and Comparators exercise

---
+++ C#-Advanced/Iterators and Comparators/Stack/CustomStack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        public Stack()
+        {
+            Information = new List<T>();
+        }
+        private List<T> Information;
+
+        public void Push(T element)
+        {
+            Information.Insert(Information.Count,element);
+        }
+        public void Pop()
+        {
+            if (Information.Count != 0)
+            {
+                Information.RemoveAt(Information.Count-1);
+            }
+            else
+            {
+                Console.WriteLine("No elements");
+            }
+
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Information.Count-1; i >=0 ; i--)
+            {
+                yield return Information[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+             return this.GetEnumerator();
+        }
+    }
+}
---
+++ C#-Advanced/Iterators and Comparators/Stack/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Stack<int> stack = new Stack<int>();
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (input.Contains("Push"))
+                {
+                    int[] split = input.Split(new string[] { "Push", ", ", " " }, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse).ToArray();
+
+                    foreach (var element in split)
+                    {
+                        stack.Push(element);
+                    }
+                }
+                else if (input == "Pop")
+                {
+                    stack.Pop();
+                }
+            }
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
