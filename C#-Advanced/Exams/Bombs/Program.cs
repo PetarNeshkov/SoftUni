251163730419879ccc7aca74c87f8317a1c60d8d
Petar <petio567@abv.bg>
2021-11-07 23:58:30 +0200

    added an exam

---
+++ C#-Advanced/Exams/Bombs/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daturaCount = 0;
+            int cherryCount = 0;
+            int smokeDecoyCount = 0;
+            int[] bombEffectArgs = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[] bombCasingsArgs = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            Queue<int> bombEffect = new Queue<int>(bombEffectArgs);
+            Stack<int> bombCasing = new Stack<int>(bombCasingsArgs);
+            bool success = false;
+            while (bombEffect.Any() && bombCasing.Any())
+            {
+                int currbombEffect = bombEffect.Peek();
+                int currbombCasing = bombCasing.Peek();
+                int sum = currbombCasing + currbombEffect;
+                switch (sum)
+                {
+                    case 40:
+                        daturaCount++;
+                        bombEffect.Dequeue();
+                        bombCasing.Pop();
+                        break;
+                    case 60:
+                        cherryCount++;
+                        bombEffect.Dequeue();
+                        bombCasing.Pop();
+                        break;
+                    case 120:
+                        smokeDecoyCount++;
+                        bombEffect.Dequeue();
+                        bombCasing.Pop();
+                        break;
+                    default:
+                        currbombCasing -= 5;
+                        bombCasing.Pop();
+                        bombCasing.Push(currbombCasing);
+                        break;
+                }
+                if (daturaCount>=3 && cherryCount>=3 && smokeDecoyCount>=3)
+                {
+                    success = true;
+                    break;
+                }
+            }
+            if (success)
+            {
+                Console.WriteLine("Bene! You have successfully filled the bomb pouch!");
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough materials to fill the bomb pouch.");
+            }
+
+            if (bombEffect.Any())
+            {
+                Console.WriteLine($"Bomb Effects: {String.Join(", ",bombEffect)}");
+            }
+            else
+            {
+                Console.WriteLine("Bomb Effects: empty");
+            }
+
+            if (bombCasing.Any())
+            {
+                Console.WriteLine($"Bomb Casings: {String.Join(", ",bombCasing)}");
+            }
+            else
+            {
+                Console.WriteLine("Bomb Casings: empty");
+            }
+
+            Console.WriteLine($"Cherry Bombs: {cherryCount}");
+            Console.WriteLine($"Datura Bombs: {daturaCount}");
+            Console.WriteLine($"Smoke Decoy Bombs: {smokeDecoyCount}");
+
+        }
+    }
+}
