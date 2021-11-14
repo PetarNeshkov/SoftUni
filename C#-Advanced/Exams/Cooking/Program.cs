624e58fa520a0435b79f84515e58eba68727abb6
Petar <petio567@abv.bg>
2021-11-14 22:14:11 +0200

    added an exam

---
+++ C#-Advanced/Exams/Cooking/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] liquids = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] ingredients = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>(ingredients);//ingredients
+            Queue<int> queue = new Queue<int>(liquids);//liqueds
+
+            int BreadCount = 0;
+            int CakeCount = 0;
+            int PastryCount = 0;
+            int FruitPieCount = 0;
+
+            while (stack.Any() && queue.Any())
+            {
+                int firstLiquid = queue.Peek();
+                int lastIngredient = stack.Peek();
+                int sum = firstLiquid + lastIngredient;
+                switch (sum)
+                {
+                    case 25:
+                        BreadCount++;
+                        stack.Pop();
+                        queue.Dequeue();
+                        break;
+                    case 50:
+                        stack.Pop();
+                        queue.Dequeue();
+                        CakeCount++;
+                        break;
+                    case 75:
+                        stack.Pop();
+                        queue.Dequeue();
+                        PastryCount++;
+                        break;
+                    case 100:
+                        stack.Pop();
+                        queue.Dequeue();
+                        FruitPieCount++;
+                        break;
+                    default:
+                        queue.Dequeue();
+                        lastIngredient +=3;
+                        stack.Pop();
+                        stack.Push(lastIngredient);
+                        break;
+                }
+            }
+            if (BreadCount!=0 && CakeCount !=0 && PastryCount !=0 && FruitPieCount!=0)
+            {
+                Console.WriteLine("Wohoo! You succeeded in cooking all the food!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to cook everything.");
+            }
+
+            if (queue.Count==0)
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Liquids left: {String.Join(", ",queue)}");
+            }
+
+            if (stack.Count==0)
+            {
+                Console.WriteLine("Ingredients left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Ingredients left: {String.Join(", ",stack)}");
+            }
+
+            Console.WriteLine($"Bread: {BreadCount}");
+            Console.WriteLine($"Cake: {CakeCount}");
+            Console.WriteLine($"Fruit Pie: {FruitPieCount}");
+            Console.WriteLine($"Pastry: {PastryCount}");
+        }
+    }
+}
