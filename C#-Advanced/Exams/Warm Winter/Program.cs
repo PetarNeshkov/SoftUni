73a466cd7233dc3111e03a506af4bf51e27dd223
Petar <petio567@abv.bg>
2021-12-07 22:48:30 +0200

    added an exam exercise

---
+++ C#-Advanced/Exams/Warm Winter/Program.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int[] hatArgs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] scarfArgs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            List<int> sets = new List<int>();
+            Stack<int> hats = new Stack<int>(hatArgs);
+            Queue<int> scarfs = new Queue<int>(scarfArgs);
+
+            while (hats.Any() && scarfs.Any())
+            {
+                int hat = hats.Peek();
+                int scarf = scarfs.Peek();
+
+                int sum = hat + scarf;
+
+                if (hat > scarf)
+                {
+                    hats.Pop();
+                    scarfs.Dequeue();
+                    sets.Add(sum);
+                }
+                else if (hat < scarf)
+                {
+                    hats.Pop();
+                }
+                else
+                {
+                    scarfs.Dequeue();
+                    hats.Push(hats.Pop() + 1);
+                }
+            }
+
+            var mostExpensiveSet = sets.OrderByDescending(x => x).FirstOrDefault();
+            Console.WriteLine($"The most expensive set is: {mostExpensiveSet}");
+            Console.WriteLine($"{String.Join(" ", sets)}");
+        }
+    }
+}
