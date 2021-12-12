e760e616c0c090c2b4f51475c0e86aa852aa67d0
Petar <petio567@abv.bg>
2021-12-12 22:49:21 +0200

    added a bonus exercise

---
+++ C#-Advanced/Bonus exercises/Fast Hash Implementation/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+namespace Fast_Hash_Implementation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 10000;
+            int containsCount = 10000;
+            List<string> list = new List<string>();
+            HashSet<string> hash = new HashSet<string>(count);
+            StringHashSet ourHash = new StringHashSet(count);
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(i.ToString());
+                hash.Add(i.ToString());
+                ourHash.Add(i.ToString());
+            }
+            Console.WriteLine("Start!");
+            var timer = Stopwatch.StartNew();
+            for (int i = 0; i < containsCount; i++)
+            {
+               bool exists= list.Contains(i.ToString());
+            }
+            timer.Stop();
+            Console.WriteLine("List sucks!");
+            Console.WriteLine(timer.ElapsedMilliseconds);
+            timer.Reset();
+            timer.Start();
+            for (int i = 0; i < containsCount; i++)
+            {
+                bool exists = hash.Contains(i.ToString());
+            }
+            timer.Stop();
+            Console.WriteLine("C# one is ok");
+            Console.WriteLine(timer.ElapsedMilliseconds);
+            timer.Reset();
+            timer.Start();
+            for (int i = 0; i < containsCount; i++)
+            {
+                bool exists = ourHash.Contains(i.ToString());
+            }
+            timer.Stop();
+            Console.WriteLine("Our is best!");
+            Console.WriteLine(timer.ElapsedMilliseconds);
+            timer.Reset();
+            timer.Start();
+        }
+        static string GenerateString(int lenght=8)
+        {
+            StringBuilder sb = new StringBuilder();
+            Random rand = new Random();
+            for (int i = 0; i <lenght ; i++)
+            {
+                sb.Append(rand.Next(0,9));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ C#-Advanced/Bonus exercises/Fast Hash Implementation/StringHashSet.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fast_Hash_Implementation
+{
+    class SetElement
+    {
+        public List<string> Keys { get; set; }
+        public string Key { get; set; }
+    }
+    public class StringHashSet
+    {
+        private SetElement[] array;
+        public StringHashSet(int capacity = 8)
+        {
+            array = new SetElement[capacity];
+        }
+        public void Add(string key)
+        {
+            int index = HashFunction(key);
+            if (array[index] != null)
+            {
+                array[index].Keys.Add(key);
+            }
+            else
+            {
+                array[HashFunction(key)] = new SetElement() { Keys = new List<string>() { key } };
+            }
+        }
+        private int HashFunction(string key)
+        {
+            UInt64 hashedValue = 3074457345618258791ul;
+            for (int i = 0; i < key.Length; i++)
+            {
+                hashedValue += key[i];
+                hashedValue *= 3074457345618258791ul;
+            }
+            UInt64 count = (ulong)array.Length;
+            return (int)(hashedValue % count);
+        
+        }
+        public bool Contains(string key)
+        {
+            int index = HashFunction(key);
+            if (array[index] != null)
+            {
+                for (int i = 0; i < array[index].Keys.Count; i++)
+                {
+                    if (array[index].Keys[i] == key)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
