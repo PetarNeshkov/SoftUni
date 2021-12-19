cfc0eec2c7266763a6667611c48c832abe4ccf1d
Petar <petio567@abv.bg>
2021-12-19 23:12:33 +0200

    added an bonus exam exercise

---
+++ C#-Advanced/Bonus exercises/SnakeGame/SoftuniExcersises-master/CSharpFundMay/ArraysDemos/ArraysDemos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace ArraysDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4 };
+
+            int sum = 0;
+            foreach (var num in arr)
+            {
+                sum += num;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
