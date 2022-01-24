a4dc7becb4724ae19a5a92afd00b1ee708cc4262
Petar <petio567@abv.bg>
2022-01-25 00:27:56 +0200

    added an Inheritance exercise

---
+++ C#-OOP/Inheritance/Stack of Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings Stack = new StackOfStrings();
+            Console.WriteLine(Stack.IsEmpty());
+            string[] array = Console.ReadLine().Split();
+            Stack<string> oldStack = new Stack<string>(array);
+            Stack.AddRange(oldStack);
+            while (Stack.Count!=0)
+            {
+                Console.WriteLine(Stack.Pop());
+            }
+        }
+    }
+}
