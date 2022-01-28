72a58f03c4a3abb21d8a7ffb68e89ed4c1a533c8
Petar <petio567@abv.bg>
2022-01-28 16:48:54 +0200

    added an OOP exercise

---
+++ INTERFACES AND ABSTRACTION/BirthdayCelebrations/Models/Robot.cs
@@ -0,0 +1,15 @@
+﻿using BirthdayCelebrations.Contracts;
+
+namespace BirthdayCelebrations.Models
+{
+    public class Robot : IIdentifiable
+    {
+        public Robot(string model, string id)
+        {
+            Model = model;
+            Id = id;
+        }
+        public string Model { get; private set; }
+        public string Id { get; private set; }
+    }
+}
