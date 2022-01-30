b29f09a22ede406e2c38134e0318dc118d35deed
Petar <petio567@abv.bg>
2022-01-31 00:01:02 +0200

    Added an OOP exercise

---
+++ C#-OOP/INTERFACES AND ABSTRACTION/BirthdayCelebrations/Models/Pet.cs
@@ -0,0 +1,17 @@
+﻿using BirthdayCelebrations.Contracts;
+
+namespace BirthdayCelebrations.Models
+{
+    public class Pet : IBirthable
+    {
+        public Pet(string name, string birtdate)
+        {
+            Name = name;
+            Birtdate = birtdate;
+        }
+
+        public string Name { get; private set; }
+
+        public string Birtdate { get; private set; }
+    }
+}
