f0b27d84cf252fbeed17a8baea874ea196276c24
Petar <petio567@abv.bg>
2022-02-01 23:44:43 +0200

    added an oop exercise

---
+++ C# OOP/INTERFACES AND ABSTRACTION/BorderControl/Core/Engine.cs
@@ -0,0 +1,54 @@
+﻿
+using BorderControl.Interfaces;
+using BorderControl.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BorderControl.Core
+{
+   public class Engine
+    {
+        private ICollection<IIdentifiable> identifiables;
+        public Engine()
+        {
+            identifiables = new List<IIdentifiable>();
+        }
+        public void Run()
+        {
+            string input;
+            while ((input=Console.ReadLine())!="End")
+            {
+                string[] split = input.Split();
+                if (split.Length == 2)
+                {
+                    AddRobot(split);
+                }
+                else if (split.Length == 3)
+                {
+                    AddCitisen(split);
+                }
+            }
+            string fakeId=Console.ReadLine();
+             identifiables.Where(x => x.Id.EndsWith(fakeId))
+                .ToList().ForEach(x => { Console.WriteLine(x.Id);});
+        }
+
+        private void AddCitisen(string[] split)
+        {
+            string name = split[0];
+            int age = int.Parse(split[1]);
+            string id = split[2];
+            IIdentifiable human = new Citizen(name,age,id);
+            identifiables.Add(human);
+        }
+
+        private void AddRobot(string[] split)
+        {
+            string model = split[0];
+            string id = split[1];
+            IIdentifiable robot = new Robot(id, model);
+            identifiables.Add(robot);
+        }
+    }
+}
---
+++ C# OOP/INTERFACES AND ABSTRACTION/BorderControl/Models/Robot.cs
@@ -0,0 +1,21 @@
+﻿using BorderControl.Interfaces;
+using System;
+
+namespace BorderControl.Models
+{
+    public class Robot : IIdentifiable
+    {
+        public Robot(string id, string model)
+        {
+            Id = id;
+            Model = model;
+        }
+        public string Id { get; private set; }
+        public string Model { get; private set; }
+        public override string ToString()
+        {
+            return $"{Id}";
+        }
+
+    }
+}
