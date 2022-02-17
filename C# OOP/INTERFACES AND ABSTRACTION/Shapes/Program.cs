42dcc95248c4fa0be839560215fc0082646cb1e9
Petar <petio567@abv.bg>
2022-02-17 23:39:13 +0200

    Added an OOP exercise

---
+++ C# OOP/INTERFACES AND ABSTRACTION/Shapes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shapes
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var radius = int.Parse(Console.ReadLine());
+            IDrawable cirle = new Circle(radius);
+
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            IDrawable rectangle = new Rectangle(width, height);
+
+            cirle.Draw();
+            rectangle.Draw();
+        }
+    }
+}
