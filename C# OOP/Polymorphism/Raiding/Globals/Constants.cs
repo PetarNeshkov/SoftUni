02c95246ac031d45b9ea2663950fc04040a90ad2
Petar <petio567@abv.bg>
2022-04-19 23:37:50 +0300

    added oop exercise

---
+++ C# OOP/Polymorphism/Raiding/Globals/Constants.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Raiding.Globals
+{
+    public class Constants
+    {
+        public const int PaladinAndWarriorPower = 100;
+        public const int RogueAndDruidPower = 80;
+
+        public const string StringOverrideDruidPaladin = "{0} - {1} healed for {2}";
+        public const string StringOverrideRogueWarrior = "{0} - {1} hit for {2} damage";
+    }
+}
---
+++ C# OOP/Polymorphism/Raiding/Program.cs
@@ -0,0 +1,12 @@
+﻿using Raiding.Core;
+namespace Raiding
+{
+  public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
