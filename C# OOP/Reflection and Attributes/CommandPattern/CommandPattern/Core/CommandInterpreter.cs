5389740f99e6e791d7363e54c1b04a43896f7d4b
Petar <petio567@abv.bg>
2022-04-28 10:26:10 +0300

    Added reflection exercise

---
+++ C# OOP/Reflection and Attributes/CommandPattern/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,35 @@
+﻿using CommandPattern.Commands;
+using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] inputTokens = args.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string commandType = inputTokens[0].ToLower();
+            string[] commandArgs = inputTokens.Skip(1).ToArray();
+
+            string result = string.Empty;
+
+            //get commandTypes
+            //create command Type instance
+            //execute command
+            Type type = Assembly
+                .GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(x=>x.Name.ToLower()==$"{commandType}Command".ToLower());
+
+            ICommand instance = (ICommand)Activator.CreateInstance(type);
+
+            result = instance.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
---
+++ C# OOP/Reflection and Attributes/CommandPattern/CommandPattern/Core/Engine.cs
@@ -0,0 +1,24 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter _commandInterpreter;
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this._commandInterpreter = commandInterpreter;
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                string result = _commandInterpreter.Read(input);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
