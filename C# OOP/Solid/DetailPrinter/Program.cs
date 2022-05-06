e0fc008a4a3a538412289e4939a5caf9142f327c
Petar <petio567@abv.bg>
2022-05-07 01:55:37 +0300

    Added Solid exercise

---
+++ C# OOP/Solid/DetailPrinter/Program.cs
@@ -0,0 +1,17 @@
+﻿using DetailPrinter.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DetailPrinter
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee("Employee");
+            var manager = new Manager("Manager", new[] { "Doc 1", "Doc 2", "Doc 3" });
+            var detailsPrinter = new DetailsPrinter(new List<Employee>(new[] { employee, manager }));
+            Console.WriteLine(detailsPrinter.PrintDetails());
+        }
+    }
+}
